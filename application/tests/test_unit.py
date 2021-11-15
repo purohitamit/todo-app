0b072706d5e861ad9133c157ff2647ca518cbfb2
Ubuntu <azureuser@myVM.5qh0wf5nsbruhlksmk54necr3c.zx.internal.cloudapp.net>
2021-11-15 14:57:16 +0000

    adding test cases

---
+++ application/tests/test_unit.py
@@ -0,0 +1,53 @@
+from flask_testing import TestCase
+from application import app, db
+from flask import url_for
+from application.models import Tasks
+
+
+class TestBase(TestCase):
+
+    def create_app(self):
+        # Defines the flask object's configuration for the unit tests
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI='sqlite:///',
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
+        )
+        return app
+
+    def setUp(self):
+        # Will be called before every test
+        # Create table schema
+        db.create_all()
+        db.session.add(Tasks(description="Run unit test"))
+        db.session.commit()
+
+
+    def tearDown(self):
+        # Will be called after every test
+        db.session.remove()
+        db.drop_all()
+
+class TestViews(TestBase):
+    # To test whether we got successful result form our route
+    def test_home_get(self):
+        response = self.client.get(url_for("home"))
+        self.assert200(response)
+
+    def test_create_task_get(self):
+        response = self.client.get(url_for("create_task"))
+        self.assert200(response)
+    
+    def test_read_task_get(self):
+        response = self.client.get(url_for("read_tasks"))
+        self.assert200(response)
+    
+    def test_update_task_get(self):
+        response = self.client.get(url_for("update_task", id=1))
+        self.assert200(response)
+    
+    
+class TestRead(TestBase):
+    def test_read_home_task(self):
+        response = self.client.get(url_for("read_tasks"))
+        self.assertIn(b"Run unit test", response.data)
