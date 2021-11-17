6c3682da86350823b90157fd68209a936df65436
Ubuntu <azureuser@myVM.5qh0wf5nsbruhlksmk54necr3c.zx.internal.cloudapp.net>
2021-11-17 17:26:43 +0000

    adding docker container

--- application/__init__.py
+++ application/__init__.py
@@ -6,6 +6,7 @@ import uuid
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI") # Set the connection string to connect to the database
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SECRET_KEY'] = str(uuid.uuid4())
 db = SQLAlchemy(app)
